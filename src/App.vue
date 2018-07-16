6b905b261453f16a28e36c3b37c25249b0161337
xieshizhen <xieshizhen@duxze.com>
2018-07-16 19:29:15 +0800

    find a dug

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,60 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <div id="root">
+        <input id="username" v-model="username" type="text">
+        <input id="password" v-model="password" type="password">
+        <input type="submit" @click="login" value="登录">
+    </div>
 </template>
-
 <script>
-export default {
-  name: 'App',
-};
+    export default {
+        name: 'root',
+        data () {
+            return {
+                username:'',
+                password:'',
+            }
+        },
+        methods:{
+            login:function(){
+                console.log('hah');
+                this.$http.post('http://10.0.20.190:8090/v1/login') //把url地址换成你的接口地址即可
+                    .then(res => {
+                        //this.request.response = res.data
+                        this.username = res.data.data.username; //把取item的数据赋给 item: []中
+                        console.log(res.data.data.username);
+                        if (res.data.code == '0') {
+                            console.log('haha');
+                        }else{
+                            alert('数据不存在');
+                        }
+                    })
+                    .catch(err => {
+                        alert('请求失败');
+                    })
+            }
+        }
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    *{margin: 0;padding: 0;}
+    @function torem($px){//$px为需要转换的字号
+        @return $px / 100px * 1rem; //100px为根字体大小
+    }
+    ul{
+        width: 100%;
+        position: absolute;
+        bottom: 0;
+        li{
+            width: torem(187.5px);
+            float:left;
+            height: torem(98px);
+            text-align:center;
+            background: #ccc;
+        }
+    }
+    img{
+        width: torem(200px);
+        height: torem(200px);
+    }
 </style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <div>
+    <li class="item" @click="delect">{{item}}</li>
+    </div>
+</div>
+</template>
+
+<script>
+    export default {
+        name: 'todoItem',
+        props:['item','index'],
+        methods:{
+            delect(){
+                this.$emit('delect',this.index);
+            }
+        }
+    };
+</script>
+<!--scoped 局部作用修饰符，样式只在这里面使用，不加会变成全局样式-->
+<style scoped>
+    .item{
+        color: green;
+    }
+</style>
