6f021b16de016c9d98da8d5062d5fc953059f4f3
xieshizhen <xieshizhen@duxze.com>
2018-07-30 11:32:32 +0800

    Data format specified,finished the showResult function

--- src/pages/showResult.vue
+++ src/pages/showResult.vue
@@ -1,29 +1,33 @@
 <template>
-<div>
-    <el-row>
-        <el-col :span="12" :offset="6">
-            <el-card class="box-card">
-                <div class="clearfix">
-                    <span></span>
-                    <el-button style="float: right; padding: 3px 0" type="text" ></el-button>
-                </div>
-                <div class="text item">
-                </div>
-            </el-card>
+    <div>
+        <el-row v-for="(item ,index) in result"
+                :key=index
+        >
+            <el-col :span="12" :offset="6">
+                <el-card class="box-card">
+                    <div class="clearfix">
+                        <span>{{item.question}}</span>
+                    </div>
+                    <div class="text item" v-for=" (chose,key) in item.choseKey">
+                        <span>{{chose}} :</span> <span>{{item.choseValue[key]}}</span>
+                    </div>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-col :span="4" :offset="7">
+            <el-button style="width: 100%" @click="change" type="success" plain>我知道了</el-button>
         </el-col>
-    </el-row>
-    <el-button @click="change">确定</el-button>
-</div>
+    </div>
 </template>
 
 <script>
     export default {
         name: "showResult",
-        props:['voteId'],
+        props: ['voteId'],
         data() {
             return {
-                answer:[],
-                question:[],
+                result: [],
+                index: 0,
             }
         },
         created() {
@@ -32,30 +36,49 @@
         },
         methods: {
             getResult() {
-                console.log('id===', this.voteId);
                 this.axios({
                     url: 'http://10.0.20.190:8090/vote/v1/result_v1?voteId=' + this.voteId,
                     method: 'GET',
                 }).then(res => {
-                    console.log('res====', res.data);
-                    res.data.result.forEach(item => {
-                        for(var key in item){
-                            this.answer.push(key);
-                            // if()
+                    for (var i = 0; i < res.data.result.length; i++) {
+                        this.result.push({
+                                question: '',
+                                choseKey: [],
+                                choseValue: [],
+                            },
+                        )
+                        this.result[i].question = res.data.result[i].question;
+                        for (var key in res.data.result[i]) {
+                            if (key === 'question') {
+                                continue;
+                            }
+                            this.result[i].choseKey.push(key);
+                            this.result[i].choseValue.push(res.data.result[i][key]);
                         }
-                        console.log(this.answer);
-                    })
+                    }
                 }).catch(err => {
-                    console.log('err====', err);
+                    alert('Some errors have occurred on the server');
+                    this.change();
                 })
             },
             change() {
-              this.$emit('change');
+                this.$emit('change');
             },
         }
     }
 </script>
 
 <style scoped>
+    .text {
+        font-size: 11px;
+        color: dodgerblue;
+    }
 
+    .item {
+        padding: 10px 0;
+    }
+
+    .box-card {
+        width: 480px;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -12,7 +12,7 @@ export default new Router({
     routes: [
         {
           path: '/',
-          component:userCenter
+          component:login
         },
         {
            path: '/user/:username/:userId',
